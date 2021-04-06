686acdf251bca02633b691425fe796cb29253d63
krujjwal <ujjwal-kr@outlook.com>
2021-04-06 10:01:32 +0530

    new added

---
+++ week_3/day_1/alert_option2.js
@@ -0,0 +1,4 @@
+function show () {
+    alert(document.getElementById("2").value);
+    document.getElementById("1").innerHTML="your choosed size"
+}
---
+++ week_3/day_1/color_chng.js
@@ -0,0 +1,10 @@
+function changered() {
+    document.getElementById("1").style.color="red";
+}
+function changeblue() {
+    document.getElementById("2").style.color="blue";
+}
+function changegreen() {
+    document.getElementById("1").style.color="green";
+    document.getElementById("2").style.color="green";
+}
