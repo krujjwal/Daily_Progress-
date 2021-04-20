375cfe1d985debe92a86da6b37c454e8ffd81ff5
krujjwal <ujjwal-kr@outlook.com>
2021-04-20 21:22:46 +0530

    new added

---
+++ week_3/day_4/assign_js.js
@@ -0,0 +1,9 @@
+function a(){
+    document.getElementById("1").innerHTML="a";
+}
+function b(){
+    document.getElementById("2").innerHTML="b";
+}
+function c(){
+    document.getElementById("3").innerHTML="b";
+}
