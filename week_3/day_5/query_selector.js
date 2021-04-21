3f57aa4cf84f586a352e8aa0f6d5487854b507af
krujjwal <ujjwal-kr@outlook.com>
2021-04-21 12:54:35 +0530

    new added

---
+++ week_3/day_5/query_selector.js
@@ -0,0 +1,7 @@
+function changeContent(){
+    var firstDiv = document.querySelector("div");
+    firstDiv.style.color ='red';
+    var firstP = document.querySelector('p');
+    firstP.style.color = 'blue';
+    firstDiv.innerHTML= "MASAI";
+}
