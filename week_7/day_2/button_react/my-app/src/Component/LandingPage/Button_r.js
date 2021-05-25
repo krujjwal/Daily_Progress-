b090cf271951115fba6e53c64c7602ec0291dc62
krujjwal <ujjwal-kr@outlook.com>
2021-05-25 23:19:46 +0530

    new added

--- week_7/day_2/button_react/my-app/src/Component/LandingPage/Button_r.js
+++ week_7/day_2/button_react/my-app/src/Component/LandingPage/Button_r.js
@@ -8,7 +8,13 @@ export default class Button_r extends Component {
             bt2:null,
             bt3:null,
             bt4:null,
-            bt5:null
+            bt5:null,
+
+            btc1:null,
+            btc2:null,
+            btc3:null,
+            btc4:null,
+            btc5:null,
         }
     }
 
@@ -18,57 +24,98 @@ export default class Button_r extends Component {
         })
     }
 
-    handleChange2=(e)=>{
+    handleChange1=(e)=>{
         this.setState({
-            
+            [e.target.name]: e.target.value
         })
     }
 
     render() {
         return (
             <div>
+                <div>
+                    <button style={{backgroundColor:this.state.btc1}}>{this.state.bt1}</button>
+                    <button style={{backgroundColor:this.state.btc2}}>{this.state.bt2}</button>
+                </div>
+
+                <div>
+                <button style={{backgroundColor:this.state.btc3}}>{this.state.bt3}</button>
+                <button style={{backgroundColor:this.state.btc4}}>{this.state.bt4}</button>
+                </div>
+
+                <button style={{backgroundColor:this.state.btc5}}>{this.state.bt5}</button>
+                <div>
+                    <input
+                        placeholder="testing1"
+                        onChange={this.handleChange}
+                    
+                        name="bt1"
+                    />
+
+                    <input
+                        placeholder="color1"
+                        onChange={this.handleChange1}
+                    
+                        name="btc1"
+                />
+                </div>
+
+                <div>
+                    <input
+                        placeholder="testing2"
+                        onChange={this.handleChange}
+                    
+                        name="bt2"
+                />
+
+                    <input
+                        placeholder="color2"
+                        onChange={this.handleChange1}
+                    
+                        name="btc2"
+                    />
+                </div>
+                <div>
+                    <input
+                        placeholder="testing3"
+                        onChange={this.handleChange}
+                    
+                        name="bt3"
+                    />
                 
-                <button>{this.state.bt1}</button>
-                <button>{this.state.bt2}</button>
-                <button>{this.state.bt3}</button>
-                <button>{this.state.bt4}</button>
-                <button>{this.state.bt5}</button>
-                
-                <input
-                    placeholder="testing1"
-                    onChange={this.handleChange}
-                    onChange={this.handleChange2}
-                    name="bt1"
-                />
-
-                <input
-                    placeholder="testing1"
-                    onChange={this.handleChange}
-                    onChange={this.handleChange2}
-                    name="bt2"
-                />
-
-                <input
-                    placeholder="testing1"
-                    onChange={this.handleChange}
-                    onChange={this.handleChange2}
-                    name="bt3"
-                />
-
-                <input
-                    placeholder="testing1"
-                    onChange={this.handleChange}
-                    onChange={this.handleChange2}
+                    <input 
+                        placeholder ="color3"
+                        onChange ={this.handleChange1}
+                        name="btc3"
+                    />
+                </div>
+                <div>
+                <input 
+                    placeholder ="testing4"
+                    onChange ={this.handleChange}
                     name="bt4"
                 />
 
-                <input
-                    placeholder="testing1"
-                    onChange={this.handleChange}
-                    onChange={this.handleChange2}
+                <input 
+                    placeholder ="color4"
+                    onChange ={this.handleChange1}
+                    name="btc4"
+                />
+                </div>
+
+                <div>
+                <input 
+                    placeholder ="testing5"
+                    onChange ={this.handleChange}
                     name="bt5"
                 />
-                <button>aa</button>
+
+                <input 
+                    placeholder ="color5"
+                    onChange ={this.handleChange1}
+                    name="btc5"
+                />
+                </div>
             </div>
         )
     }
